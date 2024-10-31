edc1379120e7cb5dca986e77b21f197ed6db625e
Antoine SOUXDORF <antoine.souxdorf@student.junia.com>
2024-10-31 16:37:14 +0100

    detcetion balle bleue et noire

--- detection_balle.py
+++ detection_balle.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import json
 
-# Initialisation du soustracteur de fond pour détecter le mouvement
+# Initialisation du soustracteur de fond
 bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16, detectShadows=True)
 
 # Déclaration de variables globales
@@ -38,10 +38,37 @@ def select_roi(frame, max_display_size=800):
 def select_color(frame):
     global ball_color
     ball_roi = cv2.selectROI("Sélectionner la couleur de la balle", frame)
-    average_color = cv2.mean(frame[ball_roi[1]:ball_roi[1]+ball_roi[3], ball_roi[0]:ball_roi[0]+ball_roi[2]])
-    ball_color = np.array([[average_color[:3]]], dtype=np.uint8)
+    larger_roi = frame[ball_roi[1]:ball_roi[1]+ball_roi[3], ball_roi[0]:ball_roi[0]+ball_roi[2]]
+    
+    average_color_bgr = cv2.mean(larger_roi)[:3]
+    ball_color_bgr = np.array([[average_color_bgr]], dtype=np.uint8)
+    ball_color_hsv = cv2.cvtColor(ball_color_bgr, cv2.COLOR_BGR2HSV)
+
+    color_name = input("Entrez le nom de cette couleur de balle : ")
+
+    with open("color.json", "r") as file:
+        color_data = json.load(file) if file else {}
+    color_data[color_name] = ball_color_hsv[0][0].tolist()
     with open("color.json", "w") as file:
-        json.dump(ball_color.tolist(), file)
+        json.dump(color_data, file)
+    ball_color = np.array([[ball_color_hsv[0][0]]], dtype=np.uint8)
+
+# Charger une couleur de balle par son nom
+def load_ball_color():
+    global ball_color
+    try:
+        with open("color.json", "r") as file:
+            colors_data = json.load(file)
+        
+        color_name = input(f"Entrez le nom de la couleur à charger parmi : {', '.join(colors_data.keys())} : ")
+
+        if color_name in colors_data:
+            ball_color = np.array([[colors_data[color_name]]], dtype=np.uint8)
+            print(f"Couleur chargée pour : {color_name}")
+        else:
+            print("Nom de couleur non trouvé.")
+    except FileNotFoundError:
+        print("Fichier color.json introuvable. Veuillez d'abord sauvegarder une couleur avec la touche c.")
 
 # Application de la ROI
 def apply_roi(frame):
@@ -51,11 +78,20 @@ def apply_roi(frame):
     return frame
 
 # Masque de couleur pour isoler la balle
-def apply_color_mask(frame, tolerance=6):
+def apply_color_mask(frame, tolerance=20):
     global ball_color
-    lower = np.array([ball_color[0][0][0] - tolerance, ball_color[0][0][1] - tolerance, ball_color[0][0][2] - tolerance])
-    upper = np.array([ball_color[0][0][0] + tolerance, ball_color[0][0][1] + tolerance, ball_color[0][0][2] + tolerance])
-    return cv2.inRange(frame, lower, upper)
+    if ball_color is None:
+        return np.zeros(frame.shape[:2], dtype=np.uint8)
+
+    # Convertir la frame en HSV
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Extraire la couleur de la balle en HSV
+    lower = np.array([ball_color[0][0][0] - tolerance, max(ball_color[0][0][1] - tolerance, 0), max(ball_color[0][0][2] - tolerance, 0)])
+    upper = np.array([ball_color[0][0][0] + tolerance, min(ball_color[0][0][1] + tolerance, 255), min(ball_color[0][0][2] + tolerance, 255)])
+
+    # Créer le masque de couleur
+    return cv2.inRange(hsv_frame, lower, upper)
 
 # Soustraction de fond
 def background_subtraction(frame, remove_shadow=True):
@@ -64,42 +100,52 @@ def background_subtraction(frame, remove_shadow=True):
         _, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
     return mask
 
-# Fonction principale pour détecter la balle noire
-def detect_black_ball(frame, roi):
+# Fonction principale pour détecter la balle
+def detect_ball(roi):
+    global combined_mask
     # Soustraction de fond
-    motion_mask = background_subtraction(roi)
+    motion_mask = background_subtraction(roi, remove_shadow=True)
 
     # Masque de couleur
     color_mask = apply_color_mask(roi) if ball_color is not None else np.zeros_like(motion_mask)
-
+    
     # Combinaison des masques
     combined_mask = cv2.bitwise_and(motion_mask, color_mask)
     combined_mask = cv2.GaussianBlur(combined_mask, (5, 5), 0)
 
     # Détection de cercles (forme circulaire)
-    circles = cv2.HoughCircles(combined_mask, cv2.HOUGH_GRADIENT, dp=1.2, minDist=50,
-                               param1=50, param2=30, minRadius=20, maxRadius=100)
+    circles = cv2.HoughCircles(combined_mask, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100,
+                               param1=30, param2=30, minRadius=20, maxRadius=150)
     if circles is not None:
         x, y, radius = circles[0][0].astype(int)
         return x, y, radius
     return None
 
 # Chargement de la vidéo
-video_path = 'video/video_balle_noire.mp4'
+video_path = 'video/video_balle_bleue.mp4'
 cap = cv2.VideoCapture(video_path)
 
-# Chargement de la ROI et couleur si disponibles
+if not cap.isOpened():
+    print("Erreur : Impossible de lire la vidéo.")
+    exit()
+
+""" #Récupérer les propriétés de la vidéo
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# # Définir le codec et initialiser le VideoWriter pour sauvegarder la vidéo avec les détections
+output_path = 'video_apres_detection/video_detection_balle_bleue.mp4'
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec pour MP4
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height)) """
+
+
+# Chargement de la ROI si disponible
 try:
     with open("roi.json", "r") as file:
         roi1 = tuple(json.load(file))
 except FileNotFoundError:
     print("Sélection de la ROI nécessaire.")
-
-try:
-    with open("color.json", "r") as file:
-        ball_color = np.array(json.load(file))
-except FileNotFoundError:
-    print("Sélection de la couleur de la balle nécessaire.")
 
 # Boucle principale
 while True:
@@ -107,31 +153,43 @@ while True:
     if not ret:
         break
 
-    # Sélection de la ROI au premier passage
     if roi1 is None:
         select_roi(frame)
     roi_frame = apply_roi(frame)
 
-    # Sélection de la couleur de la balle
-    if ball_color is None:
-        select_color(frame)
-    # Détection de la balle noire
-    ball_position = detect_black_ball(frame, roi_frame)
-    if ball_position:
-        x, y, radius = ball_position
-        cv2.circle(frame, (x + roi1[0], y + roi1[1]), radius, (0, 255, 0), 2)
-        cv2.circle(frame, (x + roi1[0], y + roi1[1]), 2, (0, 0, 255), 3)
-        print(f"Balle détectée aux coordonnées : ({x + roi1[0]}, {y + roi1[1]}), Rayon : {radius}")
+    if ball_color is not None and roi1 is not None:
+        ball_position = detect_ball(roi_frame)
+        if ball_position:
+            x, y, radius = ball_position
+            cv2.circle(frame, (x + roi1[0], y + roi1[1]), radius, (0, 255, 0), 2)
+            cv2.circle(frame, (x + roi1[0], y + roi1[1]), 2, (0, 0, 255), 3)
+            print(f"Balle détectée aux coordonnées : ({x + roi1[0]}, {y + roi1[1]}), Rayon : {radius}")
+        else:
+            print("Balle non détectée.")
+        cv2.imshow("Masque de mouvement", resize_for_display(background_subtraction(roi_frame)))
+        cv2.imshow("Masque de couleur", resize_for_display(apply_color_mask(roi_frame)))
+        cv2.imshow("Masque combine", resize_for_display(combined_mask))
 
-    # Affichage des masques et de la détection
-    cv2.imshow("Flux vidéo - Balle détectée", resize_for_display(frame))
-    cv2.imshow("Masque de Mouvement", resize_for_display(background_subtraction(roi_frame)))
-    cv2.imshow("Masque de Couleur", resize_for_display(apply_color_mask(roi_frame)))
+    else:
+        print("Sélection de la couleur de la balle nécessaire. Appuyez sur 'c' pour sélectionner la couleur.")
+    
+    #Sauvegarde de la vidéo avec les détections
+    #out.write(frame)
+    cv2.imshow("Flux vidéo", resize_for_display(frame))
+
 
     # Contrôles
-    key = cv2.waitKey(30)
+    key = cv2.waitKey(10)
+ 
+    if key == ord('c'):
+        select_color(roi_frame)
+    if key == ord('l'):  # 'l' pour charger une couleur spécifique
+        load_ball_color()
     if key == ord('q'):
         break
 
 cap.release()
+#out.release()
 cv2.destroyAllWindows()
+
+# print(f"Vidéo enregistrée sous {output_path}")
